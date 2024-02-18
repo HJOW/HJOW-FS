4cbc45d56cf6b6cbf3a47f7d88cd89b285dc04de
허진원 <hujinone11@hotmail.com>
2024-02-19 03:58:09 +0900

    Mark license text in all sources

--- WebContent/js/fscommon.js
+++ WebContent/js/fscommon.js
@@ -2,6 +2,22 @@
  * File Storage Common Scripts
  */
  
+/*
+Copyright 2024 HJOW (Heo Jin Won)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+ 
 /**
    Polyfills for classic browsers
 */ 
@@ -96,7 +112,11 @@ function FSUtilClass() {
     }
     
     this.detectLanguage = function detectLanguage() {
-        if(typeof(window.navigator.language)  != 'undefined') return window.navigator.language;
+        if(typeof(window.navigator.language)  != 'undefined') {
+            var langOne = String(window.navigator.language);
+            if(langOne.length == 2) return langOne;
+            return langOne.substring(0, 2);
+        }
         if(typeof(window.navigator.languages) != 'undefined') {
             for(var idx=0; idx<window.navigator.languages.length; idx++) {
                 var langOne = window.navigator.languages[idx];
