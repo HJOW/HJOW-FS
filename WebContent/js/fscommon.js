44ffa61056adccd5926f9dc16e876044e9a62610
허진원 <hujinone11@hotmail.com>
2024-03-30 23:23:22 +0900

    Change version codes, add bootstrap 5 (not using now)

--- WebContent/js/fscommon.js
+++ WebContent/js/fscommon.js
@@ -64,8 +64,8 @@ function FSUtilClass() {
     this.version = [];
     this.version.push(0);
     this.version.push(1);
-    this.version.push(24);
-    this.version.push(31);
+    this.version.push(25);
+    this.version.push(32);
 
     this.ctx = '';
     this.setContextPath = function(ctxPath) {
